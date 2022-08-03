1b27ec333f4bfa23c5b153f51898c56663f879f5
kthfan <3999932@gmail.com>
2022-08-03 22:50:10 +0800

    0.2.0 add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+# coding: UTF-8
+from setuptools import setup
+
+setup(
+    name="wsss",
+    version="0.2.0",
+    description="Weakly Supervised Semantic Segmentation implementations using tensorflow.",
+    long_description="See the README.md at https://github.com/kthfan/Weakly-Supervised-Semantic-Segmentation",
+    author="kthfan",
+    author_email="3999932@gmail.com",
+    url="https://github.com/kthfan/Weakly-Supervised-Semantic-Segmentation",
+    packages=["wsss"]
+)
+
+
+
